125d36221d2a4fb636795a3d2da9e453dc80c152
Rajesh Kotian <50308869+rkneo@users.noreply.github.com>
2020-09-03 19:16:17 +1000

    Update justhacks.py

--- justhacks.py
+++ justhacks.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#chunk huge dataframes
 def chunk_d(filename,df,FILE_CHUNK_SIZE):
     row=0
     while len(df) >= row :
@@ -14,5 +15,20 @@ def chunk_d(filename,df,FILE_CHUNK_SIZE):
         z = zipfile.ZipFile(filename+'.zip','w')
         z.write(filename, compress_type=zipfile.ZIP_DEFLATED)
         z.close()
+
+#split Json files        
+def split_json(jsontxt,sales_file):
+    record_chunks = 10000
+    json_obj_list = json.loads(jsontxt.decode("utf-8"))
+    json_chunk_list = [json_obj_list[i:i+record_chunks] for i  in range(0, len(json_obj_list), record_chunks)]
+    i=0
+    filelist =[]
+    for json_obj in json_chunk_list:
+        filename= os.path.join(tempfile.tempdir,'{0}-part-{1:0>5}'.format(sales_file, i) +'.json')
+        filelist.append(filename)
+        with open(filename, 'w') as out_json_file:
+             json.dump(json_obj, out_json_file,indent=4)
+        i= i+1
+    return json_chunk_list            
 
 
