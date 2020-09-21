d9a3a20bbae0d02ee17b41294074f9b950385301
Rajesh Kotian <50308869+rkneo@users.noreply.github.com>
2020-09-21 10:23:14 +1000

    Update justhacks.py

--- justhacks.py
+++ justhacks.py
@@ -10,16 +10,15 @@ import zipfile
 
 #chunk huge dataframes
 def chunk_d(filename,df,FILE_CHUNK_SIZE):
+    FILE_CHUNK_SIZE = 10000
     row=0
-    while len(df) >= row :
-        increment_row = FILE_CHUNK_SIZE + row
-        row +=1 if row > 0 else row
-        df_subset = df[row:increment_row]
-        filename= os.path.join(tempfile.tempdir,'{0}-part-{1:0>5}'.format(filename, i) +'.json')
+    while row < len(sales_df):
+        sales_df_subset = sales_df[row:row+FILE_CHUNK_SIZE]
+        filename= os.path.join(tempfile.tempdir,'{0}-part-{1:0>5}'.format(sales_file, i) +'.json')
         with open(filename, 'w') as out_json_file:
-            df_subset.to_json(path_or_buf=out_json_file, orient="records",lines=True)
+            sales_df_subset.to_json(path_or_buf=out_json_file, orient="records",lines=True)
         i+=1
-        row=increment_row
+        row += FILE_CHUNK_SIZE
         z = zipfile.ZipFile(filename+'.zip','w')
         z.write(filename, compress_type=zipfile.ZIP_DEFLATED)
         z.close()
